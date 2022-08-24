da19d01469f459780ba429fb4b99b01ea876d4a3
Bruno Justino <brunodafigueira@gmail.com>
2022-08-24 16:09:30 -0300

    Usando o JInja2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # coding: utf-8
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
-    return "Olá Mundo! Esse é meu primeiro projeto Flask!!", 200
+    #criar uma variavel com o meu nome
+    nome="Bruno da Silva Justinio"
+    return render_template("alo.html", n=nome), 200
+
+app.run()
